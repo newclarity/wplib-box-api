8e5c86a42d1eec1ba3f437cf9ebb85d8de55073a
Daryl Lozupone <daryl@newclarity.net>
2016-08-22 12:24:46 -0400

    Allow for passing of args to command line command

--- src/class-cli-interface.php
+++ src/class-cli-interface.php
@@ -5,12 +5,21 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 class WPLIB_Box_CLI_Interface {
 
-    function process_command( $command, Response $response )
+    /**
+     * @param  string   $command
+     * @param  Response $response
+     * @param  array    $args
+     * @return Response
+     */
+    function process_command($command, Response $response, $args =[])
     {
         $response = $response->withJson(['message' => 'Command not found', 'command' => $command], 500);
 
         // add check for existent command
         if (file_exists("/vagrant/scripts/guest/cli/commands/{$command}")) {
+            foreach ($args as $arg) {
+                $command .= ' ' . $arg;
+            }
             ob_start();
             passthru("box {$command}");
             $message = ob_get_clean();
