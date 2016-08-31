cb2b8fa1c691e87272e99e2e62ada87ea1ba5b24
Daryl Lozupone <daryl@newclarity.net>
2016-08-31 11:56:10 -0400

    CORS should allow access from all domains closes #3

--- public/index.php
+++ public/index.php
@@ -24,7 +24,7 @@ $app = new \Slim\App($settings);
 $app->add(function(\Slim\Http\Request $request, \Slim\Http\Response $response, $next){
 	$response = $next($request, $response);
 	return $response
-		->withHeader('Access-Control-Allow-Origin', 'http://console.wplib.box')
+		->withHeader('Access-Control-Allow-Origin', '*')
 		->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
 		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 });
